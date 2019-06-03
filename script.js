0e32aece42dba408e9aefd7ab940cb09aecfbb64
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-03 17:30:54 +0300

    Fixed

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function safeCall(f) {
-	try {
-		f();
-		return true;
-	}
-	catch {
-		return false;
-	}
+  try {
+    f();
+      return true;
+  }
+  catch {
+    return false;
+  }
 }
 
 Function.prototype.safeCall = function(...args) {
-	try {
-		return this.apply(this, args);
-	}
-	catch (ex) {
-		return null;
-	}
+  try {
+    return this.apply(this, args);
+  }
+  catch (ex) {
+    return null;
+  }
 }
